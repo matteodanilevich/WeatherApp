17bbdb5b60f72970512c276041e9661b8efe69e6
Mathew Danilevich <danilevichmathew@gmail.com>
2022-07-06 10:09:27 +0300

    Add HourlyWeather table/collection cells and extension for it Datasource and Delegate

---
+++ WeatherApp/WeatherApp/Cells/HourlyWeather/HourlyWeatherCell.swift
@@ -0,0 +1,35 @@
+//
+//  HourlyWeatherCell.swift
+//  WeatherApp
+//
+//  Created by Matthew Danilevich on 7/6/22.
+//
+
+import UIKit
+
+class HourlyWeatherCell: UITableViewCell {
+
+    @IBOutlet weak var collectionWeatherCell: UICollectionView!
+    
+    static let key = "HourlyWeatherCell"
+    
+    var dtArray: [String]!
+    var imageArray: [UIImage]!
+    var temperatureArray: [Double]!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        collectionWeatherCell.delegate = self
+        collectionWeatherCell.dataSource = self
+        
+        collectionWeatherCell.register(UINib(nibName: "HourlyWeatherCollectionCell", bundle: nil), forCellWithReuseIdentifier: HourlyWeatherCollectionCell.key)
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ WeatherApp/WeatherApp/Cells/HourlyWeatherCollection/HourlyWeatherCollectionCell.swift
@@ -0,0 +1,23 @@
+//
+//  HourlyWeatherCollectionCell.swift
+//  WeatherApp
+//
+//  Created by Matthew Danilevich on 7/6/22.
+//
+
+import UIKit
+
+class HourlyWeatherCollectionCell: UICollectionViewCell {
+
+    @IBOutlet weak var currentHour: UILabel!
+    @IBOutlet weak var currentWeatherIcon: UIImageView!
+    @IBOutlet weak var currentTemperature: UILabel!
+    
+    static let key = "HourlyWeatherCollectionCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
