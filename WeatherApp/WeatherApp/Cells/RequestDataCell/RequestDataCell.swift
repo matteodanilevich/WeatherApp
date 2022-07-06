dce59e2711e7fff512e316e914a9539bbd04ec78
Mathew Danilevich <danilevichmathew@gmail.com>
2022-07-06 12:44:48 +0300

    Add cell for requested data from data base

---
+++ WeatherApp/WeatherApp/Cells/RequestDataCell/RequestDataCell.swift
@@ -0,0 +1,29 @@
+//
+//  RequestDataCell.swift
+//  WeatherApp
+//
+//  Created by Matthew Danilevich on 7/6/22.
+//
+
+import UIKit
+
+class RequestDataCell: UITableViewCell {
+
+    @IBOutlet weak var requestData: UILabel!
+    @IBOutlet weak var requestLatitude: UILabel!
+    @IBOutlet weak var requestLongitude: UILabel!
+    @IBOutlet weak var requestWeather: UILabel!
+    @IBOutlet weak var requestTemperature: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
