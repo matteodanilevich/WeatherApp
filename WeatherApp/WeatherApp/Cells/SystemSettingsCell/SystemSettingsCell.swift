6261253d52bb859be5ea131bc9e3187a6657b383
Mathew Danilevich <danilevichmathew@gmail.com>
2022-08-08 14:27:49 +0300

    Add SystemSettingsCell

---
+++ WeatherApp/WeatherApp/Cells/SystemSettingsCell/SystemSettingsCell.swift
@@ -0,0 +1,28 @@
+//
+//  SystemSettingsCell.swift
+//  WeatherApp
+//
+//  Created by Mateo Danilevich on 8/8/22.
+//
+
+import UIKit
+
+class SystemSettingsCell: UITableViewCell {
+
+    static let key = "SystemSettingsCell"
+    
+    @IBOutlet weak var systemSettingsName: UILabel!
+    @IBOutlet weak var systemSettingsIcon: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
