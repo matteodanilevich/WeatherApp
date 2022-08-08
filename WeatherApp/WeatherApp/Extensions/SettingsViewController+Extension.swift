ca97917c91e79e9f77cf49e40cfe411bd3cd9f62
Mathew Danilevich <danilevichmathew@gmail.com>
2022-08-08 18:56:31 +0300

    Start doing settings view: add nessessary controllers and prescribed extensions for them

--- WeatherApp/WeatherApp/Extensions/SettingsViewController+Extension.swift
+++ WeatherApp/WeatherApp/Extensions/SettingsViewController+Extension.swift
@@ -9,15 +9,47 @@ import Foundation
 import UIKit
 
 extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        arraySettings.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        if let cell = tableView.dequeueReusableCell(withIdentifier: SettingsViewCell.key) as? SettingsViewCell {
+            cell.settingsLabel.text = NSLocalizedString(arraySettings[indexPath.row], comment: "")
+            return cell
+        }
+        return UITableViewCell()
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch settingsType(rawValue: indexPath.row) {
+        case .requestList:
+            if let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "RequestViewController") as? RequestViewController {
+                
+                present(vc, animated: true)
+            }
+        case .dateFormatList:
+            if let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "DataViewController") as? DataViewController {
+                
+                present(vc, animated: true)
+            }
+        case .systemList:
+            if let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SystemViewController") as? SystemViewController {
+                
+                present(vc, animated: true)
+            }
+            
+        default:
+            if let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "WeatherConditionalView") as? WeatherConditionalView {
+              
+                present(vc, animated: true)
+            }
+        }
+    }
     
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        120
+    }
     
 }
