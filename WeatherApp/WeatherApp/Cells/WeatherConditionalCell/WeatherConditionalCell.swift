df048f2495720f86ab5845deec066082909d9e46
Mathew Danilevich <danilevichmathew@gmail.com>
2022-08-08 14:14:28 +0300

    Add WeatherConditionalCell

---
+++ WeatherApp/WeatherApp/Cells/WeatherConditionalCell/WeatherConditionalCell.swift
@@ -0,0 +1,26 @@
+//
+//  WeatherConditionalCell.swift
+//  WeatherApp
+//
+//  Created by Mateo Danilevich on 8/8/22.
+//
+
+import UIKit
+
+class WeatherConditionalCell: UITableViewCell {
+
+    @IBOutlet weak var weatherConditionName: UILabel!
+    @IBOutlet weak var weatherConditionIcon: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
